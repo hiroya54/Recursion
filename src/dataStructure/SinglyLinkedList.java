65279057eb8dda19289b69f1cb3448eca568ee3e
hiroya54 <hiroya.0504@icloud.com>
2023-10-17 08:47:36 +0900

    add doubleEvenNumber method

--- src/dataStructure/SinglyLinkedList.java
+++ src/dataStructure/SinglyLinkedList.java
@@ -1,5 +1,6 @@
 package dataStructure;
 
+//リストのノード
 class SinglyLinkedListNode<E>{
     public E data;
     public SinglyLinkedListNode<E> next;
@@ -9,20 +10,18 @@ class SinglyLinkedListNode<E>{
         this.next = null;
     }
 }
+//単方向リスト
+class SinglyLinkedList{
+    public SinglyLinkedListNode<Integer> head;
 
-class SinglyLinkedList<E>{
-    public SinglyLinkedListNode<E> head;
-
-    public SinglyLinkedList(SinglyLinkedListNode<E> head){
+    public SinglyLinkedList(SinglyLinkedListNode<Integer> head){
         this.head = head;
     }
-}
-
-class Solution{
+    
     public static SinglyLinkedListNode<Integer> getLinkedList(int[] arr){
         // 関数を完成させてください
     	SinglyLinkedListNode<Integer> current = new SinglyLinkedListNode<>(arr[0]);
-    	SinglyLinkedList<Integer> list = new SinglyLinkedList<>(current);
+    	SinglyLinkedList list = new SinglyLinkedList(current);
         for(int i=1;i<arr.length;i++){
         	SinglyLinkedListNode<Integer> node = new SinglyLinkedListNode<>(arr[i]);
             current.next=node;
@@ -31,7 +30,27 @@ class Solution{
 
         return list.head;
     }
+    //偶数番目のノードを2倍にして要素の後ろに追加する
+    public SinglyLinkedListNode<Integer> doubleEvenNumber(SinglyLinkedListNode<Integer> head){
+    	
+    	SinglyLinkedListNode<Integer> iterator = head;
+    	int i=0;
+    	while(iterator!=null) {
+    		SinglyLinkedListNode<Integer> current = iterator;
+			iterator = iterator.next;
+    		if(i%2==0) {    			
+    			SinglyLinkedListNode<Integer>  newNode = new SinglyLinkedListNode<>(current.data*2);
+    			SinglyLinkedListNode<Integer> tmp = current.next;
+    			current.next = newNode;
+    			newNode.next = tmp;
+    		}
+    		i++;
+    	}
+    	
+    	return head;
+    }
 }
+
 
 class Main{
 	public static void main(String[] args) {
@@ -39,7 +58,9 @@ class Main{
 		for(int i=0;i<5;i++) {
 			arr[i] = i;
 		}
-		SinglyLinkedListNode<Integer> current = Solution.getLinkedList(arr);
+		 
+		
+		SinglyLinkedListNode<Integer> current = SinglyLinkedList.getLinkedList(arr);
 		while(current!=null) {
 			System.out.println(current.data);
 			current = current.next;
