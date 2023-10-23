2e2d5e491af5a280d727c7d31b216bfa4983eae1
hiroya54 <hiroya.0504@icloud.com>
2023-10-24 08:15:51 +0900

    [add]賭けサイコロゲーム

--- src/dataStructure/Stack.java
+++ src/dataStructure/Stack.java
@@ -85,6 +85,68 @@ class Stack {
 		return res;
 		 
 	 }
+	 
+	 public static String diceStreakGamble(int[] player1, int[] player2, int[] player3, int[] player4){
+	    
+	        Stack s1 =new Stack();
+	        Stack s2 =new Stack();
+	        Stack s3 =new Stack();
+	        Stack s4 =new Stack();
+
+	        int prize1 = s1.getPrize(player1,s1);
+	        int prize2 = s2.getPrize(player2,s2);
+	        int prize3 = s3.getPrize(player3,s3);
+	        int prize4 = s4.getPrize(player4,s4);
+	        
+	        int winnerIdx = 1;
+	        int winnerPrize = prize1;
+	        Stack winnerStack = s1;
+	        if(winnerPrize<prize2){
+	            winnerIdx = 2;
+	            winnerPrize=prize2;
+	            winnerStack=s2;
+	        }
+	        if(winnerPrize<prize3){
+	            winnerIdx = 3;
+	            winnerPrize=prize3;
+	            winnerStack=s3;
+	        }
+	        if(winnerPrize<prize4){
+	            winnerIdx = 4;
+	            winnerPrize=prize4;
+	            winnerStack=s4;
+	        }
+
+	        String res = "Winner: Player " +winnerIdx+" won $"+ winnerPrize + " by rolling [";
+	        while(winnerStack.peek()!=null){
+	            res=res+winnerStack.pop();
+	            if(winnerStack.peek()!=null){
+	                res+=",";
+	            }else{
+	                res+="]";
+	                break;
+	            }
+	        }
+
+	        return res;
+	    
+	    }
+	    public int getPrize(int[] player,Stack s){
+	        int len = player.length;
+	        s.push(player[len]);
+
+	        int prize=4;
+	        
+	         for(int i=len-2;i>=0;i--){
+	            if(player[i]<=s.peek()){
+	                s.push(player[i]);
+	                prize+=4;
+	            }else{
+	                break;
+	            }
+	         } 
+	         return prize;
+	    }
 	     
 }
 
