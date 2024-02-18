ce5a22e2b282a35428a573d182f826cbf62dc9e2
hiroya54 <hiroya.0504@icloud.com>
2024-02-18 04:09:29 +0000

    [upd]ステージ2完了

--- BackendProject/VideoCompressorService/client.py
+++ BackendProject/VideoCompressorService/client.py
@@ -4,10 +4,19 @@ import sys
 import json
 
 SERVER_ADDRESS = "0.0.0.0"
-SERVER_PORT = 9002
+SERVER_PORT = 9001
 MAX_FILE_SIZE = pow(2,32)
 MAX_PACKET_SIZE = 1400
 RESPONSE_SIZE = 16
+INPUT_MEDIA_TYPE = "mp4"
+CHECK_STR_INPUT_MEDIA_TYPE = "ftyp"
+HEADER_SIZE_TOTAL = 64
+HEADER_SIZE_JSON = 16
+HEADER_SIZE_MEDIA = 1
+HEADER_SIZE_PAYLOAD = 47
+DOWNLOAD_PAHT = "client_downloads"
+MAX_PAYLOAD_SIZE = pow(2,HEADER_SIZE_PAYLOAD)
+DEFAULT_PROCESS_CHECK_TIME = 60
 
 
 def main():
@@ -19,15 +28,45 @@ def main():
         print(err)
         sys.exit(1)
     
-    file_path = input("Please enter a file path: ")
-    try:
+    input_file_path = input("Please enter a file path: ")
+    
+    print("You can select processing method.")
+    print("1: compress the video, 2:change resolution, 3:change aspect ratio, 4:convet mp4 to mp3, 5:create gif or webm")
+    while True:
+        processing_method  = int(input("Please select a processing method form above and enter a number(1 ~ 5): "))
         
-        with open(file_path, "rb") as f:
+        if processing_method in (1, 2, 3, 4, 5):
+            break
+        else :
+            print("Invalid input. Please re-enter a number(1 ~ 5): ")
+
+    resolution_width = None   
+    resolution_height = None
+    if processing_method == 2 :
+        resolution_width = int(input("Please enter width resolution: "))
+        resolution_height = int(input("Please enter height resolution: "))
+    
+    aspect_width = None
+    aspect_height = None
+    if processing_method == 3:
+        aspect_width = int(input("Please enter aspect width: "))
+        aspect_height = int(input("Please enter aspect height: "))
+        
+    start_time = None
+    end_time = None
+    if processing_method == 5 :
+        start_time = int(input("Please enter start time to create GIF and WEBM: "))
+        end_time = int(input("Please enter end time to create GIF and WEBM: "))
+
+    try:
+        # step1: 送信処理
+        with open(input_file_path, "rb") as f:
             header = f.read(12)
             file_type = header[4:8].decode("utf-8")
-            if not file_type == "ftyp":
+            if not file_type == CHECK_STR_INPUT_MEDIA_TYPE:
                 raise ValueError("This file is not mp4 file.")
-            
+
+            f.seek(0,0)
             f.seek(0, os.SEEK_END)
             # 単位はバイト
             file_size = f.tell()
@@ -36,20 +75,62 @@ def main():
             if file_size > MAX_FILE_SIZE :
                 raise Exception("File size must be below 4 GB")
             
-            request_header = file_size.to_bytes(32, "big")
+            # MMPに合わせたヘッダー及びファイル作成           
+            media_type = INPUT_MEDIA_TYPE
+            encoded_media_type = media_type.encode()
+            input_file_name = os.path.basename(input_file_path)
+            encoded_json = create_encoded_json(file_size, input_file_name, resolution_height, resolution_width, start_time, end_time, aspect_width, aspect_height, processing_method)
+    
+            payload_length = len(encoded_json) + len(encoded_media_type) +file_size
             
+            request_header = create_multiple_media_protocol_header(len(encoded_json), len(encoded_media_type), payload_length)
+            
+            # ヘッダーの送信
             sock.sendall(request_header)
             
+            # jsonファイル→メディアタイプ→ファイル本体の順にペイロードを送信
+            sock.sendall(encoded_json)
+            sock.sendall(encoded_media_type)
             data = f.read(MAX_PACKET_SIZE)
+            print("Sending...")
+            size = 0
             while data:
-                print("Sending...")
                 sock.sendall(data)
                 data = f.read(MAX_PACKET_SIZE)
-                
-            # response = sock.recv(RESPONSE_SIZE)
+            print("Complete sending file!")
             
-            # status_code = int.from_bytes(response, big)
-            # print(status_code)
+        # step2: 受信処理
+        print("Wait response from a server")
+        # レスポンスヘッダーを受け取る
+        response_header = sock.recv(HEADER_SIZE_TOTAL)
+        response_json_length = int.from_bytes(response_header[:HEADER_SIZE_JSON], "big")
+        response_media_length = int.from_bytes(response_header[HEADER_SIZE_JSON : HEADER_SIZE_JSON + HEADER_SIZE_MEDIA], "big")
+        response_payload_length = int.from_bytes(response_header[HEADER_SIZE_JSON + HEADER_SIZE_MEDIA: HEADER_SIZE_TOTAL], "big")
+        
+        # jsonを受け取り、ステータスコードを確認する。
+        response_encoded_json = sock.recv(response_json_length)
+        response_json = json.loads(response_encoded_json.decode("utf-8"))
+        response_code = response_json["status_code"]
+        print("Status code: {}".format(response_code))
+        
+        if not response_code == 200:
+            raise ValueError(response_json["error_message"])
+        
+        # 後続のペイロード受け取り
+        response_encoded_media_type = sock.recv(response_media_length)
+        response_media_type = response_encoded_media_type.decode("utf-8")  
+
+        download_file_name = create_downloads_file_name(input_file_name, processing_method, response_media_type)
+        response_file_length = response_json["file_size"]
+
+        with open(os.path.join(DOWNLOAD_PAHT, download_file_name) ,"wb+") as f:
+            total = response_file_length
+            while response_file_length > 0:
+                data = sock.recv(response_file_length if response_file_length <= MAX_PACKET_SIZE else MAX_PACKET_SIZE)
+                f.write(data)
+                response_file_length -= len(data)
+                print("Downloaded: " + str((total-response_file_length)*100 / total) + " %")
+                print("\033[1A\033[1A")
             
     except ValueError as err:
         print(err)
@@ -59,7 +140,35 @@ def main():
         
     finally:
         print("connection close")
-        sys.exit(0)
+        sock.close()
+
+def create_encoded_json(file_size, input_file_name, resolution_height, resolution_width, start_time, end_time, aspect_width, aspect_height, processing_method):
+    
+    dict_json = {
+        "file_size" : file_size,
+        "file_name" : input_file_name,
+        "processing_method" : processing_method
+        }
+    
+    if processing_method == 2:
+        dict_json["resolution_width"] = resolution_width
+        dict_json["resolution_height"] = resolution_height
+
+    if processing_method == 3:
+        dict_json["aspect_width"] = aspect_width
+        dict_json["aspect_height"] = aspect_height    
+
+    if processing_method == 5:
+        dict_json["start_time"] = start_time
+        dict_json["end_time"] = end_time
+        
+    return json.dumps(dict_json).encode()
+
+def create_multiple_media_protocol_header(json_length, media_type_length, payload_length):
+    return json_length.to_bytes(HEADER_SIZE_JSON, "big") + media_type_length.to_bytes(HEADER_SIZE_MEDIA, "big") + payload_length.to_bytes(HEADER_SIZE_PAYLOAD, "big")
+
+def create_downloads_file_name(input_file_name, processing_method, response_media_type):
+    return input_file_name.replace(("." + INPUT_MEDIA_TYPE), "") + "_" + str(processing_method) + "."+ response_media_type
     
 
 if __name__ == "__main__":
--- BackendProject/VideoCompressorService/server.py
+++ BackendProject/VideoCompressorService/server.py
@@ -2,43 +2,177 @@ import socket
 import os
 import sys
 import json
+import ffmpeg
 
 SERVER_ADDRESS = "0.0.0.0"
-SERVER_PORT = 9002
+SERVER_PORT = 9001
 MAX_FILE_SIZE = pow(2,32)
 MAX_PACKET_SIZE = 1400
 RESPONSE_SIZE = 16
-
+HEADER_SIZE_TOTAL = 64
+HEADER_SIZE_JSON = 16
+HEADER_SIZE_MEDIA = 1
+HEADER_SIZE_PAYLOAD = 47
+MAX_PAYLOAD_SIZE = pow(2,HEADER_SIZE_PAYLOAD)
+FILE_PATH = "server_downloads/"
+TMP_FILE_PATH = "server_tmp/"
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    sock.bind((SERVER_ADDRESS,SERVER_PORT))
-    
+    sock.bind((SERVER_ADDRESS,SERVER_PORT))    
     sock.listen(1)
     
     print('Starting up on {} port {}'.format(SERVER_ADDRESS, SERVER_PORT))
     
     while True:
-        
+        print("Waiting connection...")
         try:
             connection, address = sock.accept()
             print("connection from", address)
+
+            #step1: 受信処理
+            request_header = connection.recv(HEADER_SIZE_TOTAL)
+            request_json_length = int.from_bytes(request_header[:HEADER_SIZE_JSON], "big")
+            request_media_length = int.from_bytes(request_header[HEADER_SIZE_JSON : HEADER_SIZE_JSON + HEADER_SIZE_MEDIA], "big")
+            request_payload_length = int.from_bytes(request_header[HEADER_SIZE_JSON + HEADER_SIZE_MEDIA: HEADER_SIZE_TOTAL], "big")
             
-            request_header = connection.recv(32)
+            # ボディ部の受け取り
+            request_encoded_json = connection.recv(request_json_length)
+            request_json = json.loads(request_encoded_json.decode("utf-8"))
             
-            file_size = int.from_bytes(request_header, "big")
-            file_path = "downloads/vc.mp4"
-            print("file size: " + str(file_size))
-            with open(file_path, "wb+") as f:
-                while file_size > 0:
-                    data = connection.recv(file_size if file_size <= MAX_PACKET_SIZE else MAX_PACKET_SIZE)
+            request_encoded_media_type = connection.recv(request_media_length)
+            request_media_type = request_encoded_media_type.decode("utf-8")     
+            
+            request_file_length = request_json["file_size"]
+            print("file size: " + str(request_file_length))
+
+            input_file_name = FILE_PATH + request_json["file_name"]
+            with open(input_file_name, "wb+") as f:
+                total = request_file_length
+                while request_file_length > 0:
+                    data = connection.recv(request_file_length if request_file_length <= MAX_PACKET_SIZE else MAX_PACKET_SIZE)
                     f.write(data)
-                    file_size -= len(data)
-                    print("file size: " + str(file_size))
+                    request_file_length -= len(data)
+                    print("Downloaded: " + str((total-request_file_length)*100 / total) + " %")
+                    print("\033[1A\033[1A")
             
             print('Finished downloading the file from client.')
+            
+            # step2: 動画処理
+            processing_method = request_json["processing_method"]
+            output_file_name = TMP_FILE_PATH + request_json["file_name"]
+            response_media_type = "mp4"
+            
+            print("Start processing...")
+            if processing_method == 1:
+                compress_video(input_file_name, output_file_name)
+
+            elif processing_method == 2:
+                resolution_width = request_json["resolution_width"]
+                resolution_height = request_json["resolution_height"]
+                change_resolution(input_file_name, output_file_name, resolution_width, resolution_height)
+
+            elif processing_method == 3:
+                aspect_width = request_json["aspect_width"]
+                aspect_height = request_json["aspect_height"]
+                change_aspect_ratio(input_file_name, output_file_name, aspect_width, aspect_height)
+
+            elif processing_method == 4:
+                response_media_type = "mp3"
+                output_file_name = output_file_name.replace("mp4", "") + response_media_type
+                convert_mp4_to_mp3(input_file_name, output_file_name)
+
+            elif processing_method == 5:
+                response_media_type = "gif"
+                output_file_name = output_file_name.replace("mp4", "") + response_media_type
+                start_time = request_json["start_time"]
+                end_time = request_json["end_time"]
+                get_video_duration(input_file_name)
+                create_gif_or_webm(input_file_name, output_file_name, start_time, end_time)
+
+            print("Complete processing")
+            
+            # step3: 送信処理
+            with open(output_file_name, "rb") as f:
+
+                f.seek(0, os.SEEK_END)
+                response_file_length = f.tell()
+                f.seek(0,0)
+
+                status_code = 200
+                response_encoded_json = create_encoded_json(response_file_length, status_code)
+                response_encoded_media_type = response_media_type.encode()
+                response_payload_length = len(response_encoded_json) + len(response_encoded_media_type) + response_file_length
+                response_header = create_multiple_media_protocol_header(len(response_encoded_json), len(response_encoded_media_type), response_payload_length)
+                
+                connection.sendall(response_header)
+                connection.sendall(response_encoded_json)
+                connection.sendall(response_encoded_media_type)
+
+
+                response_data = f.read(MAX_PACKET_SIZE)
+                print("Sending...")
+                while response_data:
+                    connection.sendall(response_data)
+                    response_data = f.read(MAX_PACKET_SIZE)
+                print("Complete Sending file!")
+
+                print("Delete file...")
+                delete_file(input_file_name)
+                delete_file(output_file_name)
+                print("File was deleted")
+
+        except FileNotFoundError as e:
+            print(e)
+    
+        except Exception as e:
+            print(e)
+
+        except ffmpeg.Error as e:
+            print(e.stderr)
+
         finally :
             connection.close()
             
+
+def create_multiple_media_protocol_header(json_length, media_type_length, payload_length):
+    return json_length.to_bytes(HEADER_SIZE_JSON, "big") + media_type_length.to_bytes(HEADER_SIZE_MEDIA, "big") + payload_length.to_bytes(HEADER_SIZE_PAYLOAD, "big")
+
+def create_encoded_json(file_size, status_code):
+    
+    dict_json = {
+        "file_size" : file_size,
+        "status_code" : status_code
+        }
+        
+    return json.dumps(dict_json).encode()
+
+def compress_video(input_file_name, output_file_name):
+    ffmpeg.input(input_file_name).output(output_file_name, vcodec="libx264", crf=23).run(capture_stderr=True)
+
+def change_resolution(input_file_name, output_file_name, resolution_width, resolution_height):
+    ffmpeg.input(input_file_name).filter("scale", resolution_width, resolution_height).output(output_file_name).run()
+
+def change_aspect_ratio(input_file_name, output_file_name, aspect_width, aspect_height):
+    aspect_ratio = str(aspect_width) + ":" + str(aspect_height)
+    print(aspect_ratio)
+
+    ffmpeg.input(input_file_name).filter("setdar", dar=aspect_ratio).output(output_file_name).run()
+
+def convert_mp4_to_mp3(input_file_name, output_file_name):
+    ffmpeg.input(input_file_name).output(output_file_name, format="mp3").run()
+
+def create_gif_or_webm(input_file_name, output_file_name, start_time, end_time):
+    ffmpeg.input(input_file_name, ss=start_time, to=end_time).output(output_file_name, format="gif").run()         
+
+def get_video_duration(input_file_name):
+    probe = ffmpeg.probe(input_file_name)
+    duration = float(probe["streams"][0]["duration"])
+    return duration
+
+def delete_file(file_path):
+
+    os.remove(file_path)
+
 if __name__ == "__main__":
     main()
