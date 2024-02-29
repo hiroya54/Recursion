72e72b746bb5a4faea8cf967121b8f4516932d72
hiroya54 <hiroya.0504@icloud.com>
2024-03-01 00:17:35 +0900

    [add]initial commit(画面レイアウトを作成)

---
+++ BackendProject/RestaurantChainMockup/src/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <div class="d-flex flex-column align-items-center justify-content-center">
+        <h1 class="text-center mt-4">Restaurant Chain XXX</h1>
+        <div class="border col-10 px-0 pb-2">
+            <div class="bg-light border col-12">
+                <p class="m-0 py-2">Restaurant Chain Infomation</p>
+            </div>
+            <div class="mx-2 mb-0 mt-2 border-bottom">
+                <details>
+                    <summary class="bg-lightblue text-primary p-2">Restaurant Location1</summary>
+                    <div class="mb-2">
+                        <div class="mx-4">
+                            <p class="border-bottom mb-2 py-2">Restaurant Name: Restaurant XXX</p>
+                        <div>
+                        <h5>Employees:</h5>
+                        <div class="border">
+                            <p class="m-0 p-2">Employee1</p>
+                        </div>
+                        <div class="border">
+                            <p class="m-0 p-2">Employee2</p>
+                        </div>
+                    </div>
+                </details>
+            </div>
+            <div class="mx-2 mb-0 border-bottom">
+                <details>
+                    <summary class="bg-lightblue text-primary p-2">Restaurant Location2</summary>
+                        <div class="mb-2">
+                            <div class="mx-4">
+                                <p class="border-bottom mb-2 py-2">Restaurant Name: Restaurant XXX</p>
+                            <div>
+                            <h5>Employees:</h5>
+                            <div class="border">
+                                <p class="m-0 p-2">Employee1</p>
+                            </div>
+                            <div class="border">
+                                <p class="m-0 p-2">Employee2</p>
+                            </div>
+                        </div>
+                </details>
+            </div>
+        </div>
+        <h1 class="text-center mt-4">Restaurant Chain XXX</h1>
+        <div class="border col-10 px-0 pb-2">
+            <div class="bg-light border col-12">
+                <p class="m-0 py-2">Restaurant Chain Infomation</p>
+            </div>
+            <div class="mx-2 mb-0 mt-2 border-bottom">
+                <details>
+                    <summary class="bg-lightblue text-primary p-2">Restaurant Location1</summary>
+                    <div class="mb-2">
+                        <div class="mx-4">
+                            <p class="border-bottom mb-2 py-2">Restaurant Name: Restaurant XXX</p>
+                        <div>
+                        <h5>Employees:</h5>
+                        <div class="border">
+                            <p class="m-0 p-2">Employee1</p>
+                        </div>
+                        <div class="border">
+                            <p class="m-0 p-2">Employee2</p>
+                        </div>
+                    </div>
+                </details>
+            </div>
+            <div class="mx-2 mb-0 border-bottom">
+                <details>
+                    <summary class="bg-lightblue text-primary p-2">Restaurant Location2</summary>
+                        <div class="mb-2">
+                            <div class="mx-4">
+                                <p class="border-bottom mb-2 py-2">Restaurant Name: Restaurant XXX</p>
+                            <div>
+                            <h5>Employees:</h5>
+                            <div class="border">
+                                <p class="m-0 p-2">Employee1</p>
+                            </div>
+                            <div class="border">
+                                <p class="m-0 p-2">Employee2</p>
+                            </div>
+                        </div>
+                </details>
+            </div>
+        </div>
+    </div>  
+
+</body>
+</html>
